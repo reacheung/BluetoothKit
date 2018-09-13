79b20f752a50acf9a2b513ed515cdefe2204dad0
reacheung <zhanghaiquan@xingfeiinc.com>
2018-09-13 14:53:47 +0800

    Initial Commit

---
+++ BluetoothKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BluetoothKit
+//
+//  Created by reacheung on 2018/9/13.
+//  Copyright © 2018年 zhq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
